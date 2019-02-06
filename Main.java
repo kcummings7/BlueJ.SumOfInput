529077b5f61c65e42a9a0ee26056fd375a4b1b52
Katherine Cummings <bobkat1252@gmail.com>
2019-02-06 10:06:12 -0500

    SumOfInput (complete)

--- Main.java
+++ Main.java
@@ -1,9 +1,19 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
-
-    public static void main(String[] args){
-
+    Scanner input = new Scanner(System.in);
+    public void main(String[] args){
+        countUp();
+    }
+    public void countUp(){
+        System.out.println("Please input a number!");
+        int num = Integer.parseInt(input.nextLine());
+        int sum = 0;
+        for(int i=0;i<=num;i++){
+            sum=sum+i;
+        }
+        System.out.println("You sum total is "+sum+".");
     }
 }
